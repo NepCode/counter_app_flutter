75eb8c8533a8848c3e6031d4b020db81aa918fd7
Antonio Aguila <aguilantonio16@gmail.com>
2020-01-27 22:09:21 -0600

    initial commmit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/src/app.dart';
+//import 'src/app.dart';
+
+void main() {
+  runApp(new MyApp());
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget{
+
+  final styleText = new TextStyle( fontSize: 25);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App'),
+        centerTitle: true,
+        elevation: 5.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('number of clicks:', style: styleText),
+            Text('0', style: styleText),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
